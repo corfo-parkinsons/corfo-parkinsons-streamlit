5793dc25e1337fe90d6d779bea7f9c49f91d50f0
Sergio Lucero <sergiolucero@gmail.com>
2022-06-29 14:35:59 -0400

    Update 3_stats.py

--- pages/3_stats.py
+++ pages/3_stats.py
@@ -6,7 +6,7 @@ st.set_page_config(
     page_title="Estadísticas Parkinson", page_icon="⬇", layout="wide"
 )
 
-s3 = boto3.resource('s3')  # should be validated with ENV? credentials
+s3 = boto3.resource('s3', region='us-east-2')  # should be validated with ENV? credentials
 ddb = boto3.resource('dynamodb')
 
 st.write('S3:', s3)

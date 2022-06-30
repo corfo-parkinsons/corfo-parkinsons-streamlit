0c89909a60778cadab79b6dbd778bb96a25ebcc7
Sergio Lucero <sergiolucero@gmail.com>
2022-06-30 16:28:57 +0000

    lista pacientes

--- pages/3_stats.py
+++ pages/3_stats.py
@@ -39,3 +39,7 @@ st.write('-'*80)
 s3_audiolist = audio_summary()
 st.write('<H2>S3 audios</H2>')
 st.write(s3_audiolist)
+
+pat_list = scan_all('Pacientes')
+st.write('Pacientes')
+st.write(pat_list)

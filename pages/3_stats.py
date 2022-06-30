39124b5ddcd970f16fb95897f522887d1417b5b5
Sergio Lucero <sergiolucero@gmail.com>
2022-06-30 21:10:05 +0000

    simpler stats

--- pages/3_stats.py
+++ pages/3_stats.py
@@ -10,9 +10,6 @@ st.set_page_config(
 )
 
 fs = s3fs.S3FileSystem(anon=False)
-
-# Retrieve file contents.
-# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
 @st.experimental_memo(ttl=600)
 def read_file(filename):
     with fs.open(filename) as f:
@@ -20,20 +17,10 @@ def read_file(filename):
     
 s3 = boto3.resource('s3', region_name='us-east-2')  # should be validated with ENV? credentials
 ddb = boto3.resource('dynamodb', region_name='us-east-2')
-#my_bucket = s3.Bucket('quantcldata/AUDIOS')
-#s3data = my_bucket.objects.all()
 
-#st.write('S3:', s3data)
-audios = scan_all('audios')
-adata = Counter([audio['id'] for audio in audios])
+adata = Counter([audio['id'] for audio in scan_all('audios')])
 st.write('DymamoDB:', adata)
-st.write('-'*80)
 
-#s3_audiolist = audio_list()
-s3_audiolist = audio_summary()
-st.write('<H2>S3 audios</H2>')
-st.write(s3_audiolist)
+st.write('S3 audios');st.write(audio_summary())
 
-pat_list = scan_pats()
-st.write('Pacientes')
-st.write(pat_list)
+st.write('Pacientes');st.write(scan_pats())

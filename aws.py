7195e8b3e0e87204d7677845a16ffd3a369d123b
Ubuntu <ubuntu@ip-172-31-16-146.us-east-2.compute.internal>
2022-06-30 15:00:10 +0000

    audio_summary

--- aws.py
+++ aws.py
@@ -1,4 +1,5 @@
 from boto3 import client
+import pandas as pd
 
 def audio_list():
     conn = client('s3')  # again assumes boto.cfg setup, assume AWS S3
@@ -9,7 +10,10 @@ def audio_list():
 def audio_summary():
     conn = client('s3')  # again assumes boto.cfg setup, assume AWS S3
     objects = conn.list_objects(Bucket='quantcldata',Prefix='AUDIOS/AUDIO')
-    users = set([obj['Key'].split('/')[-1].split('_')[1:]
-                for obj in objects])
+    #users = set([obj['Key'].split('/')[-1].split('_')[1:]
+    #            for obj in objects])
+    cdf = pd.DataFrame(objects['Contents'])
+    cdf['user'] = cdf.Key.apply(lambnda k: k.split('/')[-1].split('_')[1:])
 
-    return users
+
+    return cdf

696ea2a3358cb6e71e0fb85ffb33bff730a7f6ef
Ubuntu <ubuntu@ip-172-31-16-146.us-east-2.compute.internal>
2022-06-30 14:57:10 +0000

    audio_summary

--- aws.py
+++ aws.py
@@ -10,6 +10,6 @@ def audio_summary():
     conn = client('s3')  # again assumes boto.cfg setup, assume AWS S3
     objects = conn.list_objects(Bucket='quantcldata',Prefix='AUDIOS/AUDIO')
     users = set([obj['Key'].split('/')[-1].split('_')[1:]
-                for obj in objects)
+                for obj in objects])
 
     return users

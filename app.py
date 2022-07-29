b8dea778ba6a10220e0ee76f23800286a916ab14
Sergio Lucero <sergiolucero@gmail.com>
2022-07-29 18:41:18 -0400

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def run():
     base = [144,815,1486,2903,3722]
     full_audio['nfa'] = full_audio['coefs'].apply(lambda c: [(cix/bix) for cix, bix in zip(c,base)])
     full_audio = full_audio.drop(columns=['coefs'])
-    st.line_chart(data=full_audio(['id','nfa']))
+    st.line_chart(data=full_audio[['id','nfa']])
     st.dataframe(full_audio)
     
 

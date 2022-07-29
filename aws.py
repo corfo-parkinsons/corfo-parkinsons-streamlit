3687de1b3cf3b29e9317a19d923e603e683fc9af
Sergio Lucero <sergiolucero@gmail.com>
2022-07-29 18:32:20 -0400

    Update aws.py

--- aws.py
+++ aws.py
@@ -54,8 +54,8 @@ def freqs(d):
     #    'F2': '1284 [M=(1160,1300), F=(1380,1820)]', 'F3': '2247 [M=(2520,3020), F=(2750,3250)]', 'F4': '3431 [M=(3700,4250), F=(4050,4550)]', 
     #    'F2/F1': '2.140 [a=1.6,e=3.4,6.8,2.4]'}")}
     ds = d[1:700]
-    cuts1 = [ds.index(toke) for toke in ["'F0'","'F1'","'F2'","'F3'","'F4'",]]
-    cuts2 = [ds[cut:].split("'")[3].split(' ')[0] for cut in cuts1]    #  'F1': '815 
+    cuts1 = [ds.index(toke) for toke in ["'F0'","'F1'","'F2'","'F3'","'F4'","'F0dev'", "'rapJitter']]
+    cuts2 = [float(ds[cut:].split("'")[3].split(' ')[0]) for cut in cuts1]    #  'F1': '815 
     return cuts2
 
 def audio_data(all=False):

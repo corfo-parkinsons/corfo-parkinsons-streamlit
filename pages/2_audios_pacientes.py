e9a1929e5adb7f8121d3ee590572bd7479f386bb
sergiolucero <sergiolucero@gmail.com>
2022-08-01 20:45:23 +0000

    audio_datos

--- pages/2_audios_pacientes.py
+++ pages/2_audios_pacientes.py
@@ -89,7 +89,7 @@ if selection:
 
     # leer y cruzar datos DynamoDB
     dd_df = audio_data(True)
-    st.header('audio_data');st.dataframe(audio_data)
+    st.header('audio_data');st.dataframe(dd_df)
     st.header('audio_datos');st.write(audio_datos)
 
     for _, row in audio_datos:

0fdd305488ed748cfa576fb6d34a8598623da8a0
Sergio Lucero <sergiolucero@gmail.com>
2022-08-07 09:17:07 -0400

    Update 4_new_audios.py

--- pages/4_new_audios.py
+++ pages/4_new_audios.py
@@ -5,13 +5,13 @@ import aws
 adf = scan_table('audios_pacientes')
 adf = adf[adf.id.str.contains('.mp3')]
 st.header('Audios Fase Agosto')
-ROOT = 'https://quantcldata.s3.us-east-2.amazonaws.com/AUDIOS/'
+ROOT = 'https://quantcldata.s3.us-east-2.amazonaws.com/AUDIOS'
+rt = lambda dato: "<A HREF='%s/%s'>archivo</A>" %(ROOT, dato) 
+
 for col in ['user','date','coefs']:
     adf[col] = adf.data.apply(lambda d: eval(d).get(col))
-
 adf = adf.sort_values('date', ascending=False)
-links = [f"<A HREF='{ROOT}{dato}'>archivo</A>" for data in adf.data]
-adf['link']= links
+adf['link']= [rt(dato) for dato in adf.data]
 #adf = adf.drop('data', axis=1)
 st.dataframe(adf)
 

d41fc40c204e3137e80968a9599347c802f3d514
Sergio Lucero <sergiolucero@gmail.com>
2022-08-07 05:25:31 -0400

    Update 4_new_audios.py

--- pages/4_new_audios.py
+++ pages/4_new_audios.py
@@ -5,17 +5,19 @@ import aws
 adf = scan_table('audios_pacientes')
 adf = adf[adf.id.str.contains('.mp3')]
 st.header('Audios Fase Agosto')
-
+ROOT = 'https://quantcldata.s3.us-east-2.amazonaws.com/AUDIOS/'
+WHAT = "<A HREF='%s'>archivo</A>"
 for col in ['user','date','coefs']:
     adf[col] = adf.data.apply(lambda d: eval(d).get(col))
 
 adf = adf.drop('data', axis=1)
-adf = adf.sort_values('date')
+adf = adf.sort_values('date', ascending=False)
+adf['link']=adf.data.apply(lambda d: WHAT %d)
+adf = adf.drop('date', axis=1)
 st.dataframe(adf)
 
 ## ahora los links de descarga
 #AUDIO/5221716593_JOMAX_Contacto_1357.mp3
-ROOT = 'https://quantcldata.s3.us-east-2.amazonaws.com/AUDIOS/'
 for _, row in adf.iterrows():
     url = ROOT+row['id']
     html_string = f"<A HREF='{url}'>Descargar {row['id']}</A>"

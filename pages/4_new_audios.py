c5a399a542ae9ff584bf39e71513ea1811eb6cb5
Sergio Lucero <sergiolucero@gmail.com>
2022-08-08 15:53:53 +0000

    Flask serve!

--- pages/4_new_audios.py
+++ pages/4_new_audios.py
@@ -10,12 +10,17 @@ for col in ['user','date','coefs']:
     adf[col] = adf.data.apply(lambda d: eval(d).get(col))
 
 adf = adf.drop('data', axis=1)
-st.dataframe(adf)
-
-## ahora los links de descarga
 #AUDIO/5221716593_JOMAX_Contacto_1357.mp3
 ROOT = 'https://quantcldata.s3.us-east-2.amazonaws.com/AUDIOS/'
-for _, row in adf.iterrows():
-    url = ROOT+row['id']
-    html_string = f"<A HREF='{url}'>Descargar {row['id']}</A>"
-    st.markdown(html_string, unsafe_allow_html=True)
+link = lambda row: "<A HREF='%s'>link</A>"  %(ROOT+row['id'])
+adf=adf[adf.id.str.contains('.mp3')]
+adf['Descarga'] = adf.apply(link, axis=1)
+adf = adf.drop(['id'], axis=1)
+#st.dataframe(adf)
+adf = adf.sort_values('date', ascending=False)
+## hide some!
+adf['coefs'] = [{k:v for k,v in c.items() if 'apq' not in k}
+                for c in adf.coefs]
+
+st.write(adf.to_html(escape=False, index=False), unsafe_allow_html=True)
+#    st.markdown(html_string, unsafe_allow_html=True)

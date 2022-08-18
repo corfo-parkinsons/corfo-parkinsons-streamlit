a41b11fc5f86c8b07c43f28ffc6f8f5d9d289e1d
Sergio Lucero <sergiolucero@gmail.com>
2022-08-18 18:20:03 +0000

    trim text_reports

--- AUDIOS/AUDIO/text_reports.py
+++ AUDIOS/AUDIO/text_reports.py
@@ -1,13 +1,6 @@
 from newaudio import *
+
 sd=SoundData('*Daniela*mp3')
-st.df2
-sd.df2
-for fn, row in sd.df.iterrows():
-    print(fn)
-    print(row.to_dict())
 for fn, row in sd.df.iterrows():
     print(fn)
     open(fn.replace('.mp3','.txt'),'w').write(str(row.to_dict()))
-ls
-!git add *Danie*txt
-%hist -f text_reports.py
